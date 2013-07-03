9ec5ed34a39ddbff9be622335b804aaca339e0ec
Greg Cobb and JR Boyens <pair+gcobb+jboyens@pivotallabs.com>
2013-07-03 10:41:38 -0700

    Fix dependencies in setup.py to read from requirements.txt

--- setup.py
+++ setup.py
@@ -1,9 +1,28 @@
+import re
 from setuptools import setup, find_packages
+
+requirements = []
+non_pypi_requirements = []
+
+with open("requirements.txt", 'r') as f:
+    for requirement in f:
+        requirement = requirement.strip()
+
+        if requirement.startswith("#") or not requirement:
+            continue
+        elif requirement.startswith("http"):
+            non_pypi_requirements.append(requirement)
+            requirements.append(re.match(r'.*#egg=(?P<egg>.*)', requirement).group('egg'))
+        else:
+            requirements.append(requirement)
+
 setup(
     name="jasmine-py",
     version="2.0",
 
     packages=find_packages(),
 
-    install_requires=['jasmine==2.0', 'PyYAML==3.10']
+    install_requires=requirements,
+
+    dependency_links=non_pypi_requirements
 )
