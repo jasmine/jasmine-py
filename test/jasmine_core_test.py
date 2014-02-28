2c4a2368d2dc1c6ee910358fedd6e85cdf4f043a
Greg Cobb <pair+gcobb@pivotallabs.com>
2014-02-28 10:49:05 -0800

    [#66675828] Add test from jasmine-core

---
+++ test/jasmine_core_test.py
@@ -0,0 +1,29 @@
+from pytest import raises
+import pytest
+import subprocess
+
+from jasmine_core import Core
+import os
+import pkg_resources
+
+notwin32 = pytest.mark.skipif("sys.platform == 'win32'")
+
+@notwin32
+def test_js_files():
+    files = [
+        'jasmine.js',
+        'jasmine-html.js',
+        'json2.js',
+        'boot.js'
+    ]
+
+    assert Core.js_files() == files
+
+
+def test_css_files():
+    """ Should return a list of css files that are relative to Core.path() """
+    assert ['jasmine.css'] == Core.css_files()
+
+
+def test_favicon():
+    assert os.path.isfile(pkg_resources.resource_filename('jasmine_core.images', 'jasmine_favicon.png'))
