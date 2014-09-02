304633d40ab986efc45e2408e9c47d6dbe65f973
Greg Cobb and Matt Hess <mhess@pivotal.io>
2014-09-02 16:36:36 -0700

    Refactors console parser so it doesn't break when core reporter changes
    - This will prevent jasmine-ci from breaking in the future when the
      reporter changes, unless it is a field that we specifically care about
    
      [finishes #78053928]

--- jasmine/console.py
+++ jasmine/console.py
@@ -123,12 +123,21 @@ class ResultList(list):
         return filter(lambda x: x.status == status, self)
 
 
+RESULT_FIELDS = ['status', 'fullName', 'failedExpectations', 'id', 'description']
+
 class Parser(object):
     def parse(self, items):
-        return ResultList([Result(**item) for item in items])
+        return ResultList([Result(**item) for item in self._filter_fields(items)])
+
+    def _filter_fields(self, raw_items):
+        filtered_items = []
+        for item in raw_items:
+            filtered_items.append(dict(((k,v) for k,v in item.items() if k in RESULT_FIELDS)))
+        return filtered_items
 
 
-class Result(namedtuple('Result', 'status fullName failedExpectations passedExpectations id description')):
-    def __new__(cls, status=None, fullName=None, failedExpectations=[], passedExpectations=[], id=None, description=None):
-        return super(Result, cls).__new__(cls, status, fullName, failedExpectations, passedExpectations, id, description)
+class Result(namedtuple('Result', RESULT_FIELDS)):
+    def __new__(cls, status=None, fullName=None, failedExpectations=[], id=None, description=None):
+        # Constructor arguments correspond to RESULT_FIELDS
+        return super(Result, cls).__new__(cls, status, fullName, failedExpectations, id, description)
 
