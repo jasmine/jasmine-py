0d6b14ed5b875f5aa524c54cc68ab71d4bbb91c6
Greg Cobb and Matt Hess <mhess@pivotal.io>
2014-09-03 12:23:47 -0700

    Replace lettuce tests with pytests
    - Lettuce does not support py3 and was not providing any value
    
    [#66741700]

---
+++ features/end_to_end_test.py
@@ -0,0 +1,49 @@
+import subprocess
+import os
+
+def test_end_to_end(tmpdir):
+    install_jasmine(tmpdir)
+    output = run_jasmine(tmpdir)
+    assert "2 specs, 0 failed" in output
+
+def install_dependency(name, path=None):
+    location = path if path else name
+    check_output("pip install {0}".format(location), shell=True)
+
+
+def install_jasmine(project_dir):
+    install_dependency('jasmine', path=os.path.abspath(os.getcwd()))
+    check_output("cd {0} && printf 'Y\nY\n' | jasmine-install".format(project_dir), shell=True)
+
+    with open("{0}/spec/javascripts/test_spec.js".format(project_dir), 'w') as f:
+        f.writelines("""
+describe("Pants", function() {
+    it("should prove the truth", function() {
+        expect(true).toBeTruthy();
+    });
+    it("should prove numbers", function() {
+        expect(1).toBe(1);
+    });
+});
+""")
+
+
+def run_jasmine(project_dir):
+    return check_output("cd {0} && jasmine-ci".format(project_dir), shell=True)
+
+
+def check_output(*popenargs, **kwargs):
+    if hasattr(subprocess, 'check_output'):
+        return subprocess.check_output(*popenargs, **kwargs)
+
+    if 'stdout' in kwargs:
+        raise ValueError('stdout argument not allowed, it will be overridden.')
+    process = subprocess.Popen(*popenargs, stdout=subprocess.PIPE, **kwargs)
+    output, unused_err = process.communicate()
+    retcode = process.poll()
+    if retcode:
+        cmd = kwargs.get("args")
+        if cmd is None:
+            cmd = popenargs[0]
+        raise subprocess.CalledProcessError(retcode, cmd, output=output)
+    return output
--- features/runtests.feature
+++
@@ -1,7 +0,0 @@
-Feature: Running tests
-
-  Scenario: Plain project
-    Given I have a project
-    And I install the jasmine distribution
-    When I run the jasmine command
-    Then I should see my tests run
