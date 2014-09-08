37510b7e347b7aed97ff3223e0aae8ffa06a41bd
Greg Cobb <gcobb@pivotal.io>
2014-09-08 11:47:34 -0700

    Creates v2.0.1 release
    [#78400180]

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open("requirements.txt", 'r') as f:
 
 setup(
     name="jasmine",
-    version="2.0.0",
+    version="2.0.1",
     url="http://pivotal.github.io/jasmine/",
     author="Pivotal Labs",
     author_email="jasmine-js@googlegroups.com",
