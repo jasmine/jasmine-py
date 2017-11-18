ca6a39baec8f47a4be0d69984669d8c778e8c60b
Steve Gravrock <sdg@panix.com>
2017-11-18 14:39:22 -0800

    Made missing geckodriver easier to diagnose in tests

--- test/integration_test.py
+++ test/integration_test.py
@@ -1,8 +1,10 @@
 # coding=utf-8
 import sys
+import subprocess
 from subprocess import Popen, PIPE
 from time import sleep
 import requests
+from pytest import fail
 
 def get_with_retries(url):
     n = 0
@@ -103,6 +105,9 @@ def test_standalone_serves_weird_encodings():
         process.terminate()
 
 def test_ci():
+    if subprocess.call('which geckodriver > /dev/null', shell=True) != 0:
+        fail('Geckodriver was not found in $PATH')
+
     process = Popen([sys.executable, '-c', 'from jasmine.entry_points import continuous_integration; continuous_integration()', '--config', 'test/fixture_files/jasmine.yml'], stdout=PIPE)
     output = process.communicate()[0]
     process.wait()
