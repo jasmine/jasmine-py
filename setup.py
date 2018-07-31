c20ccda9f2befc7c416c0b99077d29521fc989e0
Gregg Van Hove <gregg@slackersoft.net>
2018-07-30 17:42:44 -0700

    version bump to 3.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="jasmine",
-    version="3.1.1",
+    version="3.1.2",
     url="http://jasmine.github.io",
     author="Pivotal Labs",
     author_email="jasmine-js@googlegroups.com",
